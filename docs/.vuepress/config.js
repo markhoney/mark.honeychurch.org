b487c477bcb933686c538be88fec841ed393ad47
Mark Honeychurch <mark@honeychurch.org>
2021-07-26 02:21:34 +1200

    Running, but not working

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 const existsSync = require('fs');
 // const {readFileSync} = require('fs');
-// const {resolve} = require('path');
+const {resolve} = require('path');
 // const csp = readFileSync(resolve(__dirname, 'website', 'csp.txt'), 'utf8').replace("Content-Security-Policy:", "").trim().replace(/\s\s+/g, ' ');
 
 const blog = require('./website/blog.json');
@@ -21,7 +21,7 @@ module.exports = {
 	author: 'Mark Honeychurch',
 	// dest: 'docs',
 	shouldPrefetch: () => true,
-	evergreen: true,
+	theme: resolve('./docs/.vuepress/theme'),
 	themeConfig: {
 		//repo: 'markhoney/mark.honeychurch.org',
 		// docsDir: 'docs',
@@ -98,10 +98,12 @@ module.exports = {
 			content: csp
 		}]*/
 	],
-	markdown: {
-		//lineNumbers: true
-	},
-	extendMarkdown(md) { // https://prismjs.com/#languages-list
+	/* markdown: {
+		code: {
+			lineNumbers: true,
+		},
+	}, */
+	extendsMarkdown(md) { // https://prismjs.com/#languages-list
 		md.use(require('markdown-it-video'))
 			.use(require('markdown-it-fontawesome'))
 			.use(require('markdown-it-abbr'))
@@ -128,64 +130,67 @@ module.exports = {
 			}
 		}
 	},*/
-	configureWebpack: config => { // https://github.com/vuejs/vuepress/blob/master/packages/%40vuepress/core/lib/webpack/createBaseConfig.js
-		const css = config.plugins.find(plugin => plugin.constructor.name == 'MiniCssExtractPlugin');
-		if (css) {
-			css.options.filename = 'assets/css/styles.css';
-			css.options.chunkFilename = 'assets/css/[id].styles.css';
-		}
-		config.output.filename = 'assets/js/[name].js';
-		config.module.rules.find(rule => regexSame(rule.test, /\.(png|jpe?g|gif)(\?.*)?$/)).use.find(use => use.loader == 'url-loader').options.name = 'assets/img/[name].[ext]';
-		config.module.rules.find(rule => regexSame(rule.test, /\.(svg)(\?.*)?$/)).use.find(use => use.loader == 'file-loader').options.name = 'assets/img/[name].[ext]';
-		config.module.rules.find(rule => regexSame(rule.test, /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/)).use.find(use => use.loader == 'url-loader').options.name = 'assets/media/[name].[ext]';
-		config.module.rules.find(rule => regexSame(rule.test, /\.(woff2?|eot|ttf|otf)(\?.*)?$/i)).use.find(use => use.loader == 'url-loader').options.name = 'assets/fonts/[name].[ext]';
+	bundler: '@vuepress/vite',
+	bundlerConfig: {
+		/* configureWebpack: config => { // https://github.com/vuejs/vuepress/blob/master/packages/%40vuepress/core/lib/webpack/createBaseConfig.js
+			const css = config.plugins.find(plugin => plugin.constructor.name == 'MiniCssExtractPlugin');
+			if (css) {
+				css.options.filename = 'assets/css/styles.css';
+				css.options.chunkFilename = 'assets/css/[id].styles.css';
+			}
+			config.output.filename = 'assets/js/[name].js';
+			config.module.rules.find(rule => regexSame(rule.test, /\.(png|jpe?g|gif)(\?.*)?$/)).use.find(use => use.loader == 'url-loader').options.name = 'assets/img/[name].[ext]';
+			config.module.rules.find(rule => regexSame(rule.test, /\.(svg)(\?.*)?$/)).use.find(use => use.loader == 'file-loader').options.name = 'assets/img/[name].[ext]';
+			config.module.rules.find(rule => regexSame(rule.test, /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/)).use.find(use => use.loader == 'url-loader').options.name = 'assets/media/[name].[ext]';
+			config.module.rules.find(rule => regexSame(rule.test, /\.(woff2?|eot|ttf|otf)(\?.*)?$/i)).use.find(use => use.loader == 'url-loader').options.name = 'assets/fonts/[name].[ext]';
+		}, */
 	},
-	plugins: {
+	plugins: [
 		/*'@vuepress/blog': {
 			postsDir: '_posts',
 			categoryIndexPageUrl: '/category/',
 			tagIndexPageUrl: '/tag/',
 			permalink: '/blogtest/:slug'
 		},*/
-		/*'@vuepress/google-analytics': {
-			ga: 'UA-45273295-3'
-		},*/
-		'minimal-analytics': {
-			ga: 'UA-45273295-3'
-		},
-		'container': {},
-		'reading-progress': {},
-		'@silvanite/tailwind': {
+		['@vuepress/google-analytics', {
+			id: 'G-S83CDZMMH1',
+		}],
+		/* ['minimal-analytics', {
+			ga: 'UA-45273295-3',
+		}], */
+		['@vuepress/container', {type: 'tab'}],
+		// 'reading-progress',
+		/* ['@silvanite/tailwind', {
 			purgecss: {enabled: false},
-		},
+		}], */
 		// '@vuepress/medium-zoom': {},
 		// '@vuepress/pagination': {},
-		'@vuepress/register-components': {},
-		'@vuepress/pwa': {
+		'@vuepress/register-components',
+		['@vuepress/pwa', {
 			serviceWorker: false,
 			updatePopup: {
 				message: "New content is available.",
-				buttonText: "Reload"
+				buttonText: "Reload",
 			}
-		},
-		'serve': {},
+		}],
+		// 'serve',
 		/*'@vuepress/clean-urls': {
 			normalSuffix: '',
 			indexSuffix: '',
 		},*/
-		'@vuepress/search': {
-			searchMaxSuggestions: 10
-		},
-		'sitemap': {
+		['@vuepress/search', {
+			searchMaxSuggestions: 10,
+		}],
+		/* ['sitemap', {
 			hostname: site,
 			changefreq: 'weekly'
-		},
-		'feed': {
+		}],
+		['feed', {
 			canonical_base: site,
 			count: 9999
-		},
-		'reading-time': {},
-		'seo': {
+		}],
+		'reading-time', */
+		/* ['seo', {
 			description: $page => ($page.excerpt ? $page.excerpt.replace(/<(?:.|\n)*?>/gm, '').trim() : $page.frontmatter.description),
 			author: ($page, $site) => {
 				return {
@@ -194,6 +199,7 @@ module.exports = {
 				};
 			},
 			image: $page => '/media/images' + ($page.path.startsWith('/blog/skepticism/') ? ['', $page.path.split('/')[1], $page.path.split('/')[2], ($page.frontmatter.tags ? $page.frontmatter.tags[0].toString().toLowerCase() : 'thoughts')].join('/') : ($page.path.endsWith('/') ? $page.path.slice(0, -1) : $page.path)) + '.jpg',
+			*/
 			/* imageNew: ($page) => {
 				let path = '/media/images';
 				if ($page.path.startsWith('/blog/skepticism/')) {
@@ -208,7 +214,7 @@ module.exports = {
 				path += '.jpg';
 				return path;
 			}, */
-		},
-		'tabs': {}
-	}
+		/* }], */
+		// 'tabs',
+	],
 };

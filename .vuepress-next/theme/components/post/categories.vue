b7b4c7507d426ed308fb8595351e43e0ba21d75f
Mark Honeychurch <mark@honeychurch.org>
2021-08-19 21:45:38 +1200

    Formatting

--- .vuepress-next/theme/components/post/categories.vue
+++ .vuepress-next/theme/components/post/categories.vue
@@ -1,6 +1,8 @@
 <template>
 	<span v-if="categories">
-		<router-link v-for="(category, index) in categories" :key="index" :to="'/blog/category/?name=' + category">{{category}}<span v-if="index + 1 < categories.length">, </span></router-link>
+		<router-link v-for="(category, index) in categories" :key="index" :to="'/blog/category/?name=' + category">
+			{{category}}<span v-if="index + 1 < categories.length">, </span>
+		</router-link>
 	</span>
 </template>
 
--- .vuepress-next/theme/components/post/tags.vue
+++ .vuepress-next/theme/components/post/tags.vue
@@ -1,6 +1,8 @@
 <template>
 	<span v-if="tags">
-		<router-link v-for="(tag, index) in tags" :key="index" :to="'/blog/tag/?name=' + tag">{{tag}}<span v-if="index + 1 < tags.length">, </span></router-link>
+		<router-link v-for="(tag, index) in tags" :key="index" :to="'/blog/tag/?name=' + tag">
+			{{tag}}<span v-if="index + 1 < tags.length">, </span>
+		</router-link>
 	</span>
 </template>
 

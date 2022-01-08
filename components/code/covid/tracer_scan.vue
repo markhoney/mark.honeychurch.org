60fc7ef22cc9c1571e39b9f4ad64cfa90e37f959
Mark Honeychurch <mark@honeychurch.org>
2022-01-09 02:39:20 +1300

    Another fix attempt

--- components/code/covid/tracer_scan.vue
+++ components/code/covid/tracer_scan.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<client-only>
-			<qrcode-stream @decode="onDecode" />
+			<qrcode-stream @decode="onDecode"></qrcode-stream>
 		</client-only>
 		<div>{{data}}</div>
 	</div>
@@ -15,12 +15,35 @@
 		},
 		data() {
 			return {
-				data: null,
+				raw: null,
+				error: null,
 			};
 		},
 		methods: {
 			onDecode(decodedString) {
-				this.data = decodedString;
+				this.raw = decodedString;
+			},
+		},
+		computed: {
+			decoded() {
+				const prefix = 'NZCOVIDTRACER:';
+				if (!this.raw.startsWith(prefix)) {
+					this.error = 'Not an NZ COVID Tracer QR code';
+					return null;
+				}
+				const encoded = this.raw.replace(prefix, '');
+				try {
+					const decoded = atob(encoded);
+				} catch (e) {
+					this.error = 'Not valid Base64 encoded data';
+					return null;
+				}
+				const parsed = JSON.parse(decoded);
+				if (!parsed) {
+					this.error = 'Not a valid JSON object';
+					return null;
+				}
+				return parsed;
 			},
 		},
 	};
